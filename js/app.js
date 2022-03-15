57d73f5dd4a4f82bfbd5abf1a9ae9691ed31cbba
OwethuSotomela <owethusotomela@gmail.com>
2022-03-15 16:36:46 +0200

    Highling words longer than 4

--- js/app.js
+++ js/app.js
@@ -3,25 +3,29 @@ let feedback = document.querySelector(".feedback");
 let display = document.querySelector(".footer");
 let btn = document.querySelector(".analyzeBtn");
 
-let sentence = [];
 let sentenceAnalyze = inputWords.value;
 
 function theseWords() {
+  let color = "";
   if (sentenceAnalyze != "") {
     var sentenceAnalyze = inputWords.value;
   }
   if (sentenceAnalyze == "") {
     feedback.innerHTML = "Enter a sentence to analyze..!";
-  } else {
-    if (!sentence.includes(sentenceAnalyze)) {
-      sentence.push(sentenceAnalyze);
-    }
   }
 
-  console.log(sentenceAnalyze);
-  console.log(sentence);
-  display.innerHTML = sentence;
-  return sentence;
+  let myColored = sentenceAnalyze.split(" ");
+console.log(myColored);
+  for (let i = 0; i < myColored.length; i++) {
+    let newColor = myColored[i];
+    console.log(newColor)
+    if (newColor.length > 4) {
+      color += `<mark style="color: #1EC22F;">${newColor} </mark>`;
+    } else {
+      color += newColor;
+    }
+  }
+  display.innerHTML = color;
 }
 
 btn.addEventListener("click", theseWords);
