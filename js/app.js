bbc2538a7afea026cb8e9b4338bb3d2d0e60fc87
OwethuSotomela <owethusotomela@gmail.com>
2022-03-15 13:48:52 +0200

    HTML Skeleton

--- js/app.js
+++ js/app.js
@@ -1,17 +1,27 @@
 let inputWords = document.querySelector(".words");
+let feedback = document.querySelector(".feedback");
 let display = document.querySelector(".footer");
 let btn = document.querySelector(".analyzeBtn");
 
+let sentence = [];
+let sentenceAnalyze = inputWords.value;
 
+function theseWords() {
+  if (sentenceAnalyze != "") {
+    var sentenceAnalyze = inputWords.value;
+  }
+  if (sentenceAnalyze == "") {
+    feedback.innerHTML = "Enter a sentence to analyze..!";
+  } else {
+    if (!sentence.includes(sentenceAnalyze)) {
+      sentence.push(sentenceAnalyze);
+    }
+  }
 
-function theseWords(){
-alert("Hey, it's working..!!")
+  console.log(sentenceAnalyze);
+  console.log(sentence);
+  display.innerHTML = sentence;
+  return sentence;
 }
 
-
-
-
-
-
-
-btn.addEventListener("click", theseWords)
+btn.addEventListener("click", theseWords);
