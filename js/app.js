5c1a46655b34be6c228372eb25e428bb8617c4f2
OwethuSotomela <owethusotomela@gmail.com>
2022-03-15 12:26:00 +0200

    DOM elements

---
+++ js/app.js
@@ -0,0 +1,17 @@
+let inputWords = document.querySelector(".words");
+let display = document.querySelector(".footer");
+let btn = document.querySelector(".analyzeBtn");
+
+
+
+function theseWords(){
+alert("Hey, it's working..!!")
+}
+
+
+
+
+
+
+
+btn.addEventListener("click", theseWords)
