7120c0c704a2b164157265ff6dcb89ccf05dbf44
OwethuSotomela <owethusotomela@gmail.com>
2022-03-17 09:55:36 +0200

    Returning the words when unchecking the checkbox

--- js/app.js
+++ js/app.js
@@ -4,7 +4,9 @@ let display = document.querySelector(".footer");
 let btn = document.querySelector(".analyzeBtn");
 let counted = document.querySelector(".counted");
 let checkBox = document.querySelector(".hideCheck");
-let checkMe = document.querySelector(".checkMe");
+let longBtn = document.querySelector(".longBtn");
+// let checkMe = document.querySelector(".checkMe");
+// let longWord = document.querySelector(".long")
 
 let sentenceAnalyze = inputWords.value;
 
@@ -12,9 +14,10 @@ function theseWords() {
   let color = "";
 
   if (sentenceAnalyze != "") {
-    var sentenceAnalyze = inputWords.value;
-  }
-  if (sentenceAnalyze == "") {
+    var sentenceAnalyze =
+      inputWords.value[0].toUpperCase() +
+      inputWords.value.slice(1).toLowerCase();
+  } else {
     feedback.innerHTML = "Enter a sentence to analyze..!";
   }
 
@@ -24,7 +27,7 @@ function theseWords() {
     let newColor = myColored[i];
 
     if (newColor.length > 4) {
-      color += ` <mark style="color:#157F1F">  ${newColor}  </mark> `;
+      color += `<mark style="color:#157F1F">  ${newColor}  </mark>`;
     } else {
       color += newColor + " ";
     }
@@ -36,37 +39,39 @@ function theseWords() {
 }
 
 function hideWords() {
-  var checkBox = document.querySelector("input[name='hideCheck']:checked");
+  let checkBox = document.querySelector("input[name='hideCheck']:checked");
 
   if (checkBox) {
     var hideCheck = checkBox.value;
-    console.log(hideCheck);
-  }
-  if (hideCheck != "") {
-    if (sentenceAnalyze != "") {
-      var sentenceAnalyze = inputWords.value;
-    }
-    let color = "";
+    if (hideCheck != "") {
+      let color = "";
 
-    if (sentenceAnalyze != "") {
-      var sentenceAnalyze = inputWords.value;
-    }
-    if (sentenceAnalyze == "") {
-      feedback.innerHTML = "Enter a sentence to analyze..!";
-    }
-
-    let myColored = sentenceAnalyze.split(" ");
-
-    for (let i = 0; i < myColored.length; i++) {
-      let newColor = myColored[i];
-
-      if (newColor.length > 5) {
-        color += ` <mark style="color:#157F1F">  ${newColor}  </mark> `;
+      if (sentenceAnalyze != "") {
+        var sentenceAnalyze =
+          inputWords.value[0].toUpperCase() +
+          inputWords.value.slice(1).toLowerCase();
       } else {
-        color += " ";
+        feedback.innerHTML = "Enter a sentence to analyze..!";
       }
+
+      let myColored = sentenceAnalyze.split(" ");
+
+      for (let i = 0; i < myColored.length; i++) {
+        let newColor = myColored[i];
+
+        if (newColor.length > 5) {
+          color += `<mark style="color:#157F1F"> ${newColor} </mark>`;
+        } else {
+          color += " " + " ";
+        }
+      }
+      display.innerHTML = color;
     }
-    display.innerHTML = color;
+  } else {
+    display.innerHTML =
+      inputWords.value[0].toUpperCase() +
+      inputWords.value.slice(1).toLowerCase();
+    // console.log(inputWords.value);
   }
 }
 
