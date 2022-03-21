ccda628a702a38a93bba886cd87aa793417d0603
OwethuSotomela <owethusotomela@gmail.com>
2022-03-21 20:13:48 +0200

    Creating localStorage and removing logic to Factory function

--- js/app.js
+++ js/app.js
@@ -4,36 +4,18 @@ let display = document.querySelector(".footer");
 let btn = document.querySelector(".analyzeBtn");
 let counted = document.querySelector(".counted");
 let checkBox = document.querySelector(".hideCheck");
-let longWordBtn = document.querySelector(".longBtn");
 
-let sentenceAnalyze = inputWords.value;
+var sentenceStored = [];
+if (localStorage["Last 5 Sentences"]) {
+  sentenceStored = JSON.parse(localStorage.getItem("Last 5 Sentences"));
+}
+
+const wordsWidget = WordsWidget(sentenceStored);
 
 function theseWords() {
-  let color = "";
-
-  if (sentenceAnalyze != "") {
-    var sentenceAnalyze =
-      inputWords.value[0].toUpperCase() +
-      inputWords.value.slice(1).toLowerCase();
-  } else {
-    feedback.innerHTML = "Enter a sentence to analyze..!";
-  }
-
-  let myColored = sentenceAnalyze.split(" ");
-
-  for (let i = 0; i < myColored.length; i++) {
-    let newColor = myColored[i];
-
-    if (newColor.length > 4) {
-      color += `<mark style="color:#110B11">  ${newColor}  </mark>`;
-    } else {
-      color += newColor + " ";
-    }
-  }
-  display.innerHTML = color;
-
-  counted.innerHTML =
-    "You have " + sentenceAnalyze.split(" ").length + " words in this sentence";
+  let analyseSentence = inputWords.value;
+  wordsWidget.analyseWords(analyseSentence);
+  localStorage.setItem('Last 5 Sentences', JSON.stringify(wordsWidget.getSentences()));
 }
 
 function hideWords() {
@@ -46,9 +28,7 @@ function hideWords() {
       let color = "";
 
       if (sentenceAnalyze != "") {
-        var sentenceAnalyze =
-          inputWords.value[0].toUpperCase() +
-          inputWords.value.slice(1).toLowerCase();
+        var sentenceAnalyze = inputWords.value[0].toUpperCase() + inputWords.value.slice(1).toLowerCase();
       } else {
         feedback.innerHTML = "Enter a sentence to analyze..!";
       }
@@ -82,39 +62,6 @@ function hideWords() {
   }
 }
 
-function findLongestWord() {
-  if (sentenceAnalyze != "") {
-    var sentenceAnalyze =
-      inputWords.value[0].toUpperCase() +
-      inputWords.value.slice(1).toLowerCase();
-  }
-  let myColored = sentenceAnalyze.split(" ");
-  const longestWord = myColored.reduce(function (longest, currentWord) {
-    if (currentWord.length > longest.length) {
-      return currentWord;
-    } else {
-      return longest;
-    }
-  }, "");
-
-  console.log(longestWord);
-
-  let color = "";
-  for (let i = 0; i < myColored.length; i++) {
-    let newColor = myColored[i];
-    if (newColor.length > 5 ) {
-      if (newColor == longestWord) {
-        color += ` <mark style="background-color: #42CAFD"> ${longestWord} </mark>`;
-      } else {
-        color += `<mark style="color:#110B11"> ${newColor} </mark>`;
-      }
-    } else {
-      color += "";
-    }
-  }
-  display.innerHTML = color;
-}
 
 btn.addEventListener("click", theseWords);
 checkBox.addEventListener("click", hideWords);
-longWordBtn.addEventListener("click", findLongestWord);
--- js/words-widget.js
+++ js/words-widget.js
@@ -1,3 +1,55 @@
-function WordsWidget(){
+function WordsWidget(local) {
+  let sentenceList = [];
+  sentenceList = local;
+  let message = "";
+  let mySentence = "";
 
+  function analyseWords(sentence) {
+    let applyColor = "";
+    console.log(sentence);
+    if (sentence != undefined) {
+      mySentence = sentence[0].toUpperCase() + sentence.slice(1).toLowerCase();
+      if (!sentenceList.includes(mySentence)) {
+        sentenceList.push(mySentence);
+      } else {
+        message = "Enter a sentence..";
+      }
+    }
+    let splitSentence = mySentence.split(" ");
+
+    const longestWord = splitSentence.reduce(function (longest, currentWord) {
+      if (currentWord.length > longest.length) {
+        return currentWord;
+      } else {
+        return longest;
+      }
+    }, "");
+
+    for (let i = 0; i < splitSentence.length; i++) {
+      let defColor = splitSentence[i];
+
+      if (defColor.length > 4) {
+        if (defColor == longestWord) {
+          applyColor += ` <mark style="background-color: #42CAFD"> ${longestWord} </mark>`;
+        } else {
+          applyColor += `<mark style="color:#110B11"> ${defColor} </mark>`;
+        }
+      } else {
+        applyColor += defColor + " ";
+      }
+    }
+
+    display.innerHTML = applyColor;
+    counted.innerHTML =
+      "You have " + mySentence.split(" ").length + " words in this sentence";
+  }
+
+  function getSentences() {
+    return sentenceList;
+  }
+
+  return {
+    analyseWords,
+    getSentences,
+  };
 }
